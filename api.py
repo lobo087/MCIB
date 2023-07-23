3fdd6448ba125f089df1fc41632a3b71c8b168cc
lobo087 <ivreyesch@uide.edu.ec>
2023-07-23 10:46:40 -0500

    Cambios de api

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, Response, request
-from utils import get_last_price
+from utils import get_last_price,get_page,get_list_elements
 
 app = Flask(__name__)
 
@@ -9,7 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h2>Hola Ecuador!</h2>"
-
+@app.route("/page")
+def page():
+    pagina = get_page()
+    return pagina
 
 @app.route("/api/stock")
 def get_stock_price():
@@ -21,9 +24,22 @@ def get_stock_price():
         return {"company_ticker": company_ticker, "price": None, 'error': str(error)}
 
     response_raw = {"company_ticker": company_ticker, "price": last_price}
+    print("response_raw: ",response_raw)
 
     return Response(json.dumps(response_raw), mimetype='application/json')
 
+@app.route("/api/elements")
+def get_elements():
+    company_ticker = request.args.get("ticker")
+    try:
+        values = get_list_elements(ticker=company_ticker)
+    except ValueError as error:
+        return {"company_ticker": company_ticker, "Valores": None, 'error': str(error)}
+
+    response_raw = {"CompanySymbol": values[1], "Currency": values[0], "TimeZone": values[2], "PreviousPrice": values[3], "ActualPrice": values[4]}
+    print("response_raw: ", response_raw)
+
+    return Response(json.dumps(response_raw), mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=3000, debug=True)
--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ def get_last_price(ticker: str) -> float:
     Esta funcion busca usando la API de yahoo finance el ultimo precio disponible del instrumento cuyo ticker es 'ticker'.
     @:param ticker: ticker de la empresa a consultar
     """
-
     yahoo_finance_url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}"
     # yahoo_finance_url = "https://query1.finance.yahoo.com/v8/finance/chart/" + ticker  # lo mismo que arriba
     headers = {'User-agent': 'Mozilla/5.0'}
@@ -16,4 +15,100 @@ def get_last_price(ticker: str) -> float:
 
     price = r.json().get('chart').get('result')[0].get('meta').get('regularMarketPrice')
 
+    print(r.json())
     return price
+
+
+def get_list_elements(ticker: str):
+    """
+    Esta funcion busca usando la API de yahoo finance varios elementos en función de  cuyo ticker es 'ticker'.
+    @:param ticker: ticker de la empresa a consultar
+    """
+    elements = []
+    yahoo_finance_url = f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}"
+    # yahoo_finance_url = "https://query1.finance.yahoo.com/v8/finance/chart/" + ticker  # lo mismo que arriba
+    headers = {'User-agent': 'Mozilla/5.0'}
+    r = requests.get(url=yahoo_finance_url, headers=headers)
+    if r.json().get('chart') is None:
+        raise ValueError(f"ticker: '{ticker}' not found")
+    print(r.json())
+    cu = r.json().get('chart').get('result')[0].get('meta').get('currency')
+    sy = r.json().get('chart').get('result')[0].get('meta').get('symbol')
+    ex = r.json().get('chart').get('result')[0].get('meta').get('exchangeTimezoneName')
+    pc = r.json().get('chart').get('result')[0].get('meta').get('previousClose')
+    pa = r.json().get('chart').get('result')[0].get('meta').get('regularMarketPrice')
+
+    for i in (cu,sy,ex,pc,pa):
+      elements.append(i)
+
+    return elements
+
+def get_page():
+    return """
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tratamiento de Datos - Página de Inicio</title>
+        <style>
+           
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #EAAA00;
+                
+            }
+            header {
+                background-color: #333;
+                color: #fff;
+                text-align: center;
+                padding: 1rem;
+            }
+            .container {
+                max-width: 990px;
+                margin: 0 auto;
+                padding: 2rem;
+                color: #002d72;
+            }
+            h1 {
+                color: #2d7200;
+            }
+            p {
+                line-height: 1.6;
+                color: #555;
+            }
+            footer {
+                background-color: #333;
+                color: #fff;
+                text-align: center;
+                padding: 1rem;
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Tratamiento de Datos</h1>
+        </header>
+        <div class="container">
+            <h2>Bienvenido a nuestra página de inicio</h2>
+            <p>En nuestro sitio web, encontrarás información y recursos relacionados con el tratamiento de datos.
+             Nos dedicamos a explorar las mejores prácticas para la recopilación, procesamiento, almacenamiento y análisis de datos.</p>
+            <p>Algunos temas que cubrimos incluyen:</p>
+            <ul>
+                <li>Generación de Apis</li>
+                <li>Consulta de Apis</li>
+                <li>Visualización de datos</li>
+
+            </ul>
+            <p>¡Explora nuestros artículos y recursos para obtener más información sobre este emocionante campo!</p>
+        </div>
+        <footer>
+            <p>Derechos Reservados &copy; 2023 | Tratamiento de Datos</p>
+        </footer>
+    </body>
+    </html> """
