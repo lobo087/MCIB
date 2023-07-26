b89ed8766adcaa69ce5e199130872aa03192c317
lobo087 <140168722+lobo087@users.noreply.github.com>
2023-07-25 23:44:57 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@ from api import get_stock_price
 
 
 def test_get_stock_price():
-    cocacola_price = get_stock_price('KO').json
+    cocacola_price = get_last_price('KO').json
     print(cocacola_price)
 
     assert cocacola_price['price'] > 0
